0bae4ba8ae54d85702a77cb12ecaddb980275688
roysurya <84616228+roysurya@users.noreply.github.com>
2022-07-11 17:56:59 +0530

    Update visualizations.js

--- scripts/visualizations.js
+++ scripts/visualizations.js
@@ -1,9 +1,4 @@
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    E-MAIL:-    tummala.kethan@gmail.com
-    *****************
-*/
+
 
 //We only have to change background-color and height of the sorting element.
 
@@ -56,10 +51,3 @@ function enable_buttons()
         }
     },c_delay+=delay_time);
 }
-
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    E-MAIL:-    tummala.kethan@gmail.com
-    *****************
-*/
