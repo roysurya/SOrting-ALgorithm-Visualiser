ba971e2ddeb0cfc1c43bdcceaff42a640fff8779
roysurya <84616228+roysurya@users.noreply.github.com>
2022-07-11 17:55:55 +0530

    Update quick_sort.js

--- scripts/quick_sort.js
+++ scripts/quick_sort.js
@@ -1,9 +1,4 @@
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    E-MAIL:-    tummala.kethan@gmail.com
-    *****************
-*/
+
 
 function Quick()
 {
@@ -71,10 +66,3 @@ function quick_sort (start, end )
         quick_sort (piv_pos +1, end) ;//sorts the right side of pivot.
     }
  }
-
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    E-MAIL:-    tummala.kethan@gmail.com
-    *****************
-*/
